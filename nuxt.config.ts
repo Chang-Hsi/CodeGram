87f6488ecd42fbc0d8cf70f001d48cea6a44492f
Douli <watasiwa8531@gmail.com>
2026-07-28 13:34:05 +0800

    初步安裝插件，規劃專案結構

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+
+  devtools: {
+    enabled: true,
+  },
+
+  modules: [
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    '@nuxt/icon',
+    '@nuxt/image',
+    'shadcn-nuxt',
+  ],
+
+  css: [
+    '~/assets/css/tailwind.css',
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui',
+  },
 })
